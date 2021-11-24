38c7771c64ff319e690c01930febd6a42bf9e9a2
merlinjones10 <merlinjones10@gmail.com>
2021-11-24 17:36:52 +0000

    transpose matrix working

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
-# This is a sample Python script.
+# Take in a matrix and ''transpose/rotate? it'
+# i.e make columns rows
+# 1 2 3     1 4 7
+# 4 5 6 --> 2 5 8
+# 7 8 9     3 6 9
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# get the 1st item of every ROW, i.e array, push to new array
+#      '' 2nd
+# Loop through an array of array with a counter, fetch the nth index of the item
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+arr = [[1, 2],
+       [4, 5],
+       [7, 8],
+       [8, 5]]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-f
+def transpose_matrix(matrix):
+    new_matrix = []
+    for index in range(len(list[0])):
+        new_sub_list = []
+        for sub_index in range(len(list)):
+            new_sub_list.append(list[sub_index][index])
+            if sub_index == len(list) - 1:
+                new_list.append(new_sub_list)
+    return new_matrix
+
+
+
+
+
+
+
+
+
+
+
+# create an array, push items to that array
+# # wipe the array clean
+# test_list = []
+# nested_list = []
+# for i in range(3):
+#    nested_list.append(i)
+#
+# test_list.append(nested_list)
+# print(test_list)
