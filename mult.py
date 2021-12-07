6d356280191b5a4cdedbc5e7a7b2d7c54ae0f6c0
merlinjones10 <merlinjones10@gmail.com>
2021-12-07 20:17:27 +0000

    working with variable length matrices

--- mult.py
+++ mult.py
@@ -2,25 +2,23 @@
 # multiply the nth elements in an array together
 
 list1 = [[1, 5],
-         [0, 3]]
+         [0, 3],
+         [1, 5]]
 list2 = [[4, 3],
-         [2, 1]]
+         [2, 1],
+         [5, 3]]
 nested = []
 listFinal = []
-mySum = 0
+dot_sum = 0
 
 for _ in range(len(list1)):
-
     for i in range(len(list1)):
-
-        for x in range(len(list1)):
-            print(_, i, x)
-            mySum += list1[_][x] * list2[i][x]
-            print(list1[_][x] * list2[i][x])
-            if x == 1:
-                nested.append(mySum)
-                mySum = 0
-            if x == 1 and i == 1:
+        for x in range(len(list1[0])):
+            dot_sum += list1[_][x] * list2[i][x]
+            if x == len(list1[0]) - 1:
+                nested.append(dot_sum)
+                dot_sum = 0
+            if x == len(list1[0]) - 1 and i == len(list1) - 1:
                 listFinal.append(nested)
                 nested = []
 
